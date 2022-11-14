117357d8f982ea3678420e3c4075dc8ffa8b37ab
matheussouzalima <matholaslima@gmail.com>
2022-11-14 13:58:07 -0300

    finalização rotas bonus e sanitização de dados

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import cors from "cors";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import joi from 'joi';
 import dayjs from "dayjs";
 import dotenv from "dotenv";
+import {stripHtml} from "string-strip-html";
 const app = express();
 dotenv.config();
 
@@ -30,7 +31,7 @@ async function verificaOnline(user){
 
 app.post("/participants",(req,res)=>{
     const userSchema = joi.string().required();
-    let name = req.body.name;
+    let name = stripHtml(req.body.name).result.trim();
 
     let validation = userSchema.validate(name,{abortEarly:false});
 
@@ -80,8 +81,10 @@ app.post("/messages",async (req,res)=>{
         text: joi.string().required()
     })
 
-    let from = req.headers.user;
-    let {to,text,type} = req.body;
+    let from = stripHtml(req.headers.user).result.trim();
+    let type = stripHtml(req.body.type).result.trim();;
+    let to = stripHtml(req.body.to).result.trim();
+    let text = stripHtml(req.body.text).result.trim();
     let object = {from,to,text,type}
 
     let validation = userSchema.validate(object);
@@ -130,14 +133,89 @@ app.get("/messages",(req,res)=>{
                 res.status(404).send();
             }
             else{
-                res.status(200).send(messages)
+                
+                messages.sort((a,b)=>{
+                    return  a._id < b._id ? -1 : 1;
+                })
+                res.status(200).send(messages);
             }
         });
     }
 })
 
+app.put("/messages/:ID",async (req,res)=>{
+    let ID = ObjectId(req.params.ID);
+    const userSchema = joi.object({
+        from: joi.string().required(),
+        to: joi.string().required(),
+        type: joi.string().required(),
+        text: joi.string().required()
+    })
+
+    let from = stripHtml(req.headers.user).result.trim();
+    let type = stripHtml(req.body.type).result.trim();;
+    let to = stripHtml(req.body.to).result.trim();
+    let text = stripHtml(req.body.text).result.trim();
+    let object = {from,to,text,type}
+    let validation = userSchema.validate(object);
+
+    if(validation.error){
+        const erros = validation.error.details.map((detail) => detail.message);
+        res.status(422).send(erros);
+        return;
+    }
+    else{
+        let online = await verificaOnline(from);
+        let valida = (type === 'message' || type === 'private_message');
+
+        if(online && valida){
+            db.collection("messages").find({_id:ID}).toArray().then((messages)=>{
+                if(messages.length === 0){
+                    res.status(404).send();
+                }
+                else{
+                    if(messages[0].from === from){
+                        db.collection("messages").updateOne({_id:ID}, {$set:{text:text}}, function(err, resa) {
+                            if (err) throw err;
+                            res.status(200).send();
+                        });
+                    }
+                    else{
+                        res.status(401).send();
+                    }
+                }
+            })
+
+        }
+        else{
+            res.status(422).send();
+        }
+
+    }
+})
+app.delete("/messages/:ID",(req,res)=>{
+    let {user} = req.headers;
+    let ID = ObjectId(req.params.ID);
+    db.collection("messages").find({_id:ID}).toArray().then((messages)=>{
+        if(messages.length === 0){
+            res.status(404).send();
+        }
+        else{
+            if(messages[0].from === user){
+                db.collection("messages").deleteOne({_id:ID}, function(err, resa) {
+                    if (err) throw err;
+                    res.status(200).send();
+                });
+            }
+            else{
+                res.status(401).send();
+            }
+        }
+    })
+
+})
 app.post("/status",(req,res)=>{
-    let user = req.headers.user;
+    let user = stripHtml(req.headers.user).result.trim();
 
     db.collection("users").find({name:user}).toArray().then(users=>{
         if(users.length === 0){
